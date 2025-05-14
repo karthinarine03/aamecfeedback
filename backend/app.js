c43c3e96d5aff8c85cd7b8a8c9cc4659b0689d3f
thiruvenkatasamyiyya <thiruvenkatasamyiyya@gmail.com>
2025-05-14 06:04:15 +0530

    clear probelm

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,14 @@ import dotenv from 'dotenv'
 import errorMiddleware from "./middleware/errorMiddleware.js"
 import courserouter from  './routers/courserouter.js'
 import studentRouter from './routers/studentRouter.js'
+import connectDatabase from './config/dbConnect.js'
 const app = express()
 
 dotenv.config({path : 'backend/config/config.env'})
 app.use(express.json());
+
+//connect db
+connectDatabase()
 
 app.use('/api',courserouter);
 app.use('/api/v1/',studentRouter)
--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
-const connectDatabase = ()=>{
-    mongoose.connect('mongodb+srv://thiruvenkatasamyiyya:thiruvenkatam@cluster0.i16j9.mongodb.net/feedback')
+const connectDatabase = async()=>{
+    await mongoose.connect('mongodb+srv://thiruvenkatasamyiyya:thiruvenkatam@cluster0.i16j9.mongodb.net/feedback')
     .then((con)=>{
         console.log(`Mongodb is conntected successfully in ${con.connection.host}`);
     })
